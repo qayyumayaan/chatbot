d992f39b4206a9cedc872b592cb14540eb0ff530
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-07 19:46:16 -0400

    Have check for other user

--- src/render.js
+++ src/render.js
@@ -2,10 +2,17 @@ window.addEventListener('DOMContentLoaded', () => {
     const submitButton = document.querySelector('input[type="submit"]');
     const inputBox = document.querySelector('#input');
   
+    const processInputText = (text) => {
+      if (text === 'password') {
+        console.log('yay!');
+      }
+    };
+  
     const printInputText = () => {
       const text = inputBox.value;
       console.log(text);
-      inputBox.value = ''; // Clear the input box
+      processInputText(text);
+      inputBox.value = ''; 
     };
   
     submitButton.addEventListener('click', printInputText);
