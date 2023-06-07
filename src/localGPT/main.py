7240bf8852b4360d5abe5235608108dd9a05c78f
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-07 19:52:25 -0400

    Create main.py

---
+++ src/localGPT/main.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    key = "password"  # Predefined key
+
+    # Prompt the user to enter a word
+    user_input = input("Enter a word: ")
+
+    # Check if the user input matches the key
+    if user_input == key:
+        print("Correct!")
+    else:
+        print("That's not it!")
+
+if __name__ == "__main__":
+    main()
