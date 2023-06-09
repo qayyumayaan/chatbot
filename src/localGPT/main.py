2f00fa6c70043b26f043fee68ffcfb4a07b4ade3
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-08 23:04:06 -0400

    Update main.py

--- src/localGPT/main.py
+++ src/localGPT/main.py
@@ -1,16 +1,18 @@
 import sys
 
+
 def main():
     key = "password"  # Predefined key
 
-    # Prompt the user to enter a word
-    user_input = input()
+    while True:
+        # Prompt the user to enter a word
+        user_input = input()
 
-    # Check if the user input matches the key
-    if user_input == key:
-        print("Correct!")
-    else:
-        print("That's not it!")
+        # Check if the user input matches the key
+        if user_input == key:
+            print("Correct!")
+        else:
+            print("That's not it!")
 
 if __name__ == "__main__":
     main()
